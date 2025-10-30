organicmaps/organicmaps

Add OM version to uploaded OSM notes
Signed-off-by: Alexander Borsuk <me@alex.bio>

libs/editor/server_api.cpp
@@ -8,6 +8,7 @@
 #include "base/math.hpp"
 #include "base/string_utils.hpp"
 #include "base/timer.hpp"
+#include "platform/platform.hpp"
 
 #include <algorithm>
 #include <sstream>
@@ -119,9 +120,9 @@ void ServerApi06::CloseChangeSet(uint64_t changesetId) const
 uint64_t ServerApi06::CreateNote(ms::LatLon const & ll, std::string const & message) const
 {
   CHECK(!message.empty(), ("Note content should not be empty."));
-  std::string const params = "?lat=" + strings::to_string_dac(ll.m_lat, 7) +
-                             "&lon=" + strings::to_string_dac(ll.m_lon, 7) +
-                             "&text=" + url::UrlEncode(message + " #organicmaps " + OMIM_OS_NAME);
+  std::string const params =
+      "?lat=" + strings::to_string_dac(ll.m_lat, 7) + "&lon=" + strings::to_string_dac(ll.m_lon, 7) +
+      "&text=" + url::UrlEncode(message + " #organicmaps " + OMIM_OS_NAME + " " + GetPlatform().Version());
   OsmOAuth::Response const response = m_auth.Request("/notes" + params, "POST");
   if (response.first != OsmOAuth::HTTP::OK)
     MYTHROW(ErrorAddingNote, ("Could not post a new note:", response));

Alec-crypto-cmd/Pitdrive-os

libs/editor/server_api.cpp
@@ -8,6 +8,7 @@
 #include "base/math.hpp"
 #include "base/string_utils.hpp"
 #include "base/timer.hpp"
+#include "platform/platform.hpp"
 
 #include <algorithm>
 #include <sstream>
@@ -121,7 +122,7 @@ uint64_t ServerApi06::CreateNote(ms::LatLon const & ll, std::string const & mess
   CHECK(!message.empty(), ("Note content should not be empty."));
   std::string const params = "?lat=" + strings::to_string_dac(ll.m_lat, 7) +
                              "&lon=" + strings::to_string_dac(ll.m_lon, 7) +
-                             "&text=" + url::UrlEncode(message + " #CoMaps " + OMIM_OS_NAME);
+                             "&text=" + url::UrlEncode(message + " #CoMaps " + OMIM_OS_NAME + " " + GetPlatform().Version());
   OsmOAuth::Response const response = m_auth.Request("/notes" + params, "POST");
   if (response.first != OsmOAuth::HTTP::OK)
     MYTHROW(ErrorAddingNote, ("Could not post a new note:", response));